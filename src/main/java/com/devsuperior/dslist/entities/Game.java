3fa6c7f88a76e737c80bdfe3294b5d5d4685b615
SabrinaMarquesF <sabrinamferraz20@gmail.com>
2023-05-17 22:43:20 -0300

    Seed dos Games

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -21,23 +21,29 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String plataform;
-	private String imUrl;
-	private String shortDescriptionString;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String plataform, String imUrl,
-			String shorDescriptionString, String longDescription) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataform = plataform;
-		this.imUrl = imUrl;
-		this.shortDescriptionString = shorDescriptionString;
+		this.platforms = platforms;
+		this.score = score;
+		this.imgUrl = imgUrl;
+		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
 	}
 
@@ -73,28 +79,37 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlataform() {
-		return plataform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlataform(String plataform) {
-		this.plataform = plataform;
+	public void setPlatforms(String plataforms) {
+		this.platforms = plataforms;
+	}
+	
+
+	public Double getScore() {
+		return score;
 	}
 
-	public String getImUrl() {
-		return imUrl;
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
-	public void setImUrl(String imUrl) {
-		this.imUrl = imUrl;
+	public String getImgUrl() {
+		return imgUrl;
 	}
 
-	public String getShortDescriptionString() {
-		return getShortDescriptionString();
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
 	}
 
-	public void setShortDescriptionString(String shorDescriptionString) {
-		this.shortDescriptionString = shorDescriptionString;
+	public String getShortDescription() {
+		return getShortDescription();
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
 	}
 
 	public String getLongDescription() {
